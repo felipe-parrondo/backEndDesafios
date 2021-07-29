8ceed8fcdcd1ccb2aaf71a79f3099751fde8668a
Felipe Parrondo Bastos <42877008+felipe-parrondo@users.noreply.github.com>
2021-07-28 22:31:17 -0300

    desafio 12 - corregido

--- desafioClase8y9/version2/server.js
+++ desafioClase8y9/version2/server.js
@@ -14,26 +14,25 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded());
 
 
-//app.set("view engine", "handlebars");
-//app.engine("handlebars", handlebars());
+app.engine("handlebars", handlebars());
+app.set("view engine", "handlebars");
 
-app.set("views", "./views")
-app.set("view engine", "pug")
+app.use(express.static("./views"));
 
 
 routerProductos.get("/", (req, res) =>{
-    res.send(Productos.leer())
-})
+    res.send(Productos.leer());
+});
 
 routerProductos.get("/:id", (req, res) => {
-    let tempId = req.params.id
-    res.send(Productos.leerId(tempId))
-})
+    let tempId = req.params.id;
+    res.send(Productos.leerId(tempId));
+});
 
 routerProductos.post("/", (req, res) => {
-    Productos.guardar(req.body.title, req.body.price, req.body.thumbnail)
-    res.redirect("/productos/carga")
-})
+    Productos.guardar(req.body.title, req.body.price, req.body.thumbnail);
+    res.redirect("/productos/vista");
+});
 
 routerProductos.put("/:id", (req, res) => {
     let tempObj = {
@@ -41,14 +40,14 @@ routerProductos.put("/:id", (req, res) => {
         price: req.body.price,
         thumbnail: req.body.thumbnail,
     }
-    Productos.update(req.params.id, tempObj)
+    Productos.update(req.params.id, tempObj);
     
-    res.send("Actualizado de forma correcta!")
-})
+    res.send("Actualizado de forma correcta!");
+});
 
 routerProductos.delete("/:id", (req, res) => {
     res.send(Productos.delete(req.params.id));
-})
+});
 
 //USO HTML
 
@@ -62,15 +61,24 @@ app.get("/productos/vista", (req, res) => {
     res.render("productos", {productos: productos, verif: verif});
 })
 
-app.get("/productos/carga", (req, res) => {
-    res.render("ingreso.pug")
-})
-
 
 
 app.use("/api/productos", routerProductos);
 
 const PORT = 8080;
-app.listen(PORT, () => {
+http.listen(PORT, () => {
     console.log("Servidor escuchando en el puerto " + PORT);
+});
+
+io.on("connection", (socket) => {
+    console.log("Usuario conectado");
+    //socket.emit("miMensaje", "Conectado!")
+});
+
+io.on("cargaProductos", (tempObj) => {
+    try{
+        Productos.guardar(tempObj.title, tempObj.price, tempObj.thumbnail);
+    } catch(e) {
+        console.log(e)
+    }
 })
